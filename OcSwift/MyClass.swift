18e981fc39df68dae5b35f28869f75955dc1cedc
mumuyang1 <843624084@qq.com>
2015-12-21 23:37:39 +0800

    can call swift code in oc code

---
+++ OcSwift/MyClass.swift
@@ -0,0 +1,15 @@
+//
+//  constant.swift
+//  OcSwift
+//
+//  Created by Yanzi Li on 12/21/15.
+//  Copyright © 2015 Yanzi Li. All rights reserved.
+//
+
+import Foundation
+
+@objc struct Constants {
+
+    static let Welcome = "kWelcomeNotif";
+
+};
